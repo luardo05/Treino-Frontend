e38844ed95ddc3a928c917842723a58604810214
Luiz Eduardo de Almeida <101152317+luardo05@users.noreply.github.com>
2026-01-17 13:47:22 -0300

    Update API base URL to include '/api' path

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://treino-api-eg0s.onrender.com',
+    baseURL: 'https://treino-api-eg0s.onrender.com/api',
 });
 
 export default api;
